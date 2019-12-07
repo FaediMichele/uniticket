938b1257d4260b15df8b54d34931d2e0d7f200e1
EmanueleBenedettini <emanuele.benedettini@studio.unibo.it>
2019-12-07 14:43:20 +0100

    Created basic folders

---
+++ db/database.php
@@ -0,0 +1,13 @@
+<?php
+	class DatabaseHelper{
+		private $db;
+
+		public function __construct($servername, $username, $password, $dbname){
+			$this->db = new mysqli($servername, $username, $password, $dbname);
+			if($this->db->connect_error){
+				die("Connection failed to db");
+			}
+		}
+
+	}
+?>
