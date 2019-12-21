80878de86b1b0b9458350715c6a6855756d065cb
Michele Faedi <Michele.Faedi@studio.unibo.it>
2019-12-21 16:55:34 +0100

    gestione corretta delle cartelle e file

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["titolo"] = "UniTicket - Home";
+$templateParams["nome"] = "home.php";
+//$templateParams["eventi"] = $dbh->getRandomPosts(2);
+
+require("template/base.php");
+?>
---
+++ template/base.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>e quel - Home</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+</head>
+<body>
+    <header>
+        <h1> e quel ticket</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Eventi</a></li>
+            <li><a href="#">Archivio</a></li>
+            <li><a href="#">Contatti</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+    </nav>
+    <main>
+        
+    </main>
+    <aside>
+        
+    </aside>
+    <footer>
+        <p>Sede legale, e info azienda?</p>
+    </footer>
+</body>
+</html>
