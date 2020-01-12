c3ee98aa29c05ba9d9516e1927070cdf75b2dc8a
Michele Faedi <Michele.Faedi@studio.unibo.it>
2020-01-12 14:55:07 +0100

    .

---
+++ template/notFound.php
@@ -0,0 +1,13 @@
+<div id="main" class="container">
+    <article>
+        <div>
+            <p id="oops">OOPS, SOMETHING GO WRONG</p>
+        </div>
+        <div>
+            <p id="code">404</P>
+        </div>
+        <div >
+            <p id="notFound">PAGE NOT FOUND</p>
+        </div>
+    </article>
+</div>
