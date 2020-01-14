c3b3e964e781a1089cfcd29efbfd4be2da3f748c
cricas <cristian.casadei6@studio.unibo.it>
2020-01-14 11:08:51 +0100

    ciao

---
+++ template/home.php
@@ -0,0 +1,45 @@
+                <div class="row">
+                    <!-- inizio post nella home -->
+                    <div class="col-12 col-xl-3 home-post">
+                        <header>
+                            <img src="./img/locandina.jpg">
+                            <h2>gio 12 dic</h2>
+                            <h4>energy <span>sala 2</span></h4>
+                            <h3>happy new year 2020 <span>Max Pezzali + Finley</span></h3>
+                        </header>
+                    </div>
+                    <div class="col-12 col-xl-3 home-post">
+                        <header>
+                            <img src="./img/locandina.jpg">
+                            <h2>gio 12 dic</h2>
+                            <h4>energy <span>sala 2</span></h4>
+                            <h3>happy new year 2020 <span>Max Pezzali + Finley</span></h3>
+                        </header>
+                    </div>
+
+                    <div class="col-12 col-xl-3 home-post">
+                        <header>
+                            <img src="./img/locandina.jpg">
+                            <h2>gio 12 dic</h2>
+                            <h4>energy <span>sala 2</span></h4>
+                            <h3>happy new year 2020 <span>Max Pezzali + Finley</span></h3>
+                        </header>
+                    </div>
+                    <div class="col-12 col-xl-3 home-post">
+                        <header>
+                            <img src="./img/locandina.jpg">
+                            <h2>gio 12 dic</h2>
+                            <h4>energy <span>sala 2</span></h4>
+                            <h3>happy new year 2020 <span>Max Pezzali + Finley</span></h3>
+                        </header>
+                    </div>
+                    <div class="col-12 col-xl-3 home-post">
+                        <header>
+                            <img src="./img/locandina.jpg">
+                            <h2>gio 12 dic</h2>
+                            <h4>energy <span>sala 2</span></h4>
+                            <h3>happy new year 2020 <span>Max Pezzali + Finley</span></h3>
+                        </header>
+                    </div>
+                    <!-- fine post nella home -->
+                </div>
