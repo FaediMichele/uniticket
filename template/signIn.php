d8d367fff6bd08cbd46c5bb6a7f44cc6e1b69383
cricas <cristian.casadei6@studio.unibo.it>
2020-01-14 15:56:03 +0100

    fix login

--- template/signIn.php
+++ template/signIn.php
@@ -1,44 +1,43 @@
-			<!-- username -->
-            <div class="row">
-                <div class="col-1"></div>
-                <div class="col-10">
-                    <form id="form-username">
-                        <input type="username" name="siteusername" placeholder="Username" class="input input-max-width"
-                            id="username" />
-                    </form>
+            <!-- username -->
+            <form>
+                <div class="row">
+                    <div class="col-1"></div>
+                    <div class="col-10">
+                            <input class="input" type="username" name="siteusername" placeholder="Username" class="input input-max-width"
+                                id="username" />
+                    </div>
+                    <div class="col-1"></div>
                 </div>
-                <div class="col-1"></div>
-            </div>
 
-			<!-- password -->
-            <div class="row">
-                <div class="col-1"></div>
-                <div class="col-10">
-                    <form id="form-password">
-                        <input type="password" name="sitepassword" placeholder="Password" class="input input-max-width"
-                            id="password" />
-                    </form>
+                <!-- password -->
+                <div class="row">
+                    <div class="col-1"></div>
+                    <div class="col-10">
+                            <input class="input" type="password" name="sitepassword" placeholder="Password" class="input input-max-width"
+                                id="password" />
+                    </div>
+                    <div class="col-1"></div>
                 </div>
-                <div class="col-1"></div>
-            </div>
 
-			<!-- signIn -->
-            <div class="row">
-                <div class="col-1"></div>
-				<div class="col-5">
-					<input type="checkbox" name="rememberMe" value="remember">Ricordami
-				</div>
-                <div class="col-5">
-					<button class="button-orange" type="signInButton" id="signIn" name="sitesignIn" onclick="formAction(signUp.php)">Accedi</button> 
+                <!-- signIn -->
+                <div class="row">
+                    <div class="col-1"></div>
+                    <div class="col-5">
+                        <div class="row-center">
+                            <input type="checkbox" name="rememberMe" value="remember"><span class="text-gray"><span class="text-gray"> Ricordami</span>
+                        </div>
+                    </div>
+                    <div class="col-5">
+                        <button class="button-orange" type="signInButton" id="signIn" name="sitesignIn" onclick="formAction(signUp.php)">Accedi</button> 
+                    </div>
+                    <div class="col-1"></div>
                 </div>
-                <div class="col-1"></div>
-            </div>
-
+            </form>
 			<!-- text -->
             <div class="row">
                 <div class="col-1"></div>
                 <div class="col-10">
-                    <p>Non hai ancora un account?</p>
+                    <h6 class="text-center">Non hai ancora un account?</p>
                 </div>
                 <div class="col-1"></div>
             </div>
@@ -47,7 +46,7 @@
             <div class="row">
                 <div class="col-1"></div>
                 <div class="col-10">
-					<button class="button-orange" type="signUpButton" id="signUp" name="sitesignUp" value="Go to signUp" onclick="formAction(signUp.php)" >Registrati</button> 
+					<button class="button-white" type="signUpButton" id="signUp" name="sitesignUp" value="Go to signUp" onclick="formAction(signUp.php)" >Registrati</button> 
                 </div>
                 <div class="col-1"></div>
             </div>
