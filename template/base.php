b7080db41cb8482172f3401d28c74ee898604e42
cricas <cristian.casadei6@studio.unibo.it>
2020-01-14 19:12:27 +0100

    la mamma di quelli di twitter è una cattiva persona

--- template/base.php
+++ template/base.php
@@ -98,7 +98,7 @@
                     </div>
                     <div class="col-8">
                         <div class="row">
-                            <div class="col-10 mx-auto mb-2">
+                            <div class="col-11 mx-auto mb-2 p-0">
                                 <a href="#">
                                     <img src="./img/logo.png" height="70" alt="">
                                 </a>
