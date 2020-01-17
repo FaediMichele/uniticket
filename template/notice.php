ebf56e3be27625ddf8bad88bc4ead6ef54d17af6
cricas <cristian.casadei6@studio.unibo.it>
2020-01-17 18:08:07 +0100

    sistemate in tutte le pagine i contenuti

--- template/notice.php
+++ template/notice.php
@@ -1,39 +1,43 @@
-<div class="container inline-block" id="notices">
-    <div class="row noti">
-        <div class="noti-event-image col-3 p-0"> 
-            <img class="img-responsive" src="./img/locandina.jpg" alt="image of the event"/>
-        </div>
-        <div class="noti-event-info col-7 padding-0">
-            <p class="noti-event-date ">GIO. 12. DIC</p>
-            <p class="noti-event-name text-truncate">HAPPY NEW YEAR 2020</p>
-            <p class="noti-event-description text-truncate">Lorem ipsum as as ssss ciao ciccio</p>
-        </div> 
-        <div class="col-2 reset">
-            <div class="row reset">
-                <div class="col-12 align-self-start reset noti-notice-date">12:03</div>
-                <div class="col-12 align-self-center align-self-end reset">
-                    <div class="badge-notify">
-                        2
+<div class="col-11">
+    <div class="row contenuti">
+        <div class="container inline-block" id="notices">
+            <div class="row noti">
+                <div class="noti-event-image col-3 p-0">
+                    <img class="img-responsive" src="./img/locandina.jpg" alt="image of the event" />
+                </div>
+                <div class="noti-event-info col-7 padding-0">
+                    <p class="noti-event-date ">GIO. 12. DIC</p>
+                    <p class="noti-event-name text-truncate">HAPPY NEW YEAR 2020</p>
+                    <p class="noti-event-description text-truncate">Lorem ipsum as as ssss ciao ciccio</p>
+                </div>
+                <div class="col-2 reset">
+                    <div class="row reset">
+                        <div class="col-12 align-self-start reset noti-notice-date">12:03</div>
+                        <div class="col-12 align-self-center align-self-end reset">
+                            <div class="badge-notify">
+                                2
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="row noti">
-        <div class="noti-event-image col-3 p-0"> 
-            <img class="img-responsive" src="./img/locandina.jpg" alt="image of the event"/>
-        </div>
-        <div class="noti-event-info col-7 padding-0">
-            <p class="noti-event-date ">GIO. 12. DIC</p>
-            <p class="noti-event-name text-truncate">HAPPY NEW YEAR 2020</p>
-            <p class="noti-event-description text-truncate">Lorem ipsum as as ssss ciao ciccio</p>
-        </div> 
-        <div class="col-2 reset">
-            <div class="row reset">
-                <div class="col-12 align-self-start reset noti-notice-date">12:03</div>
-                <div class="col-12 align-self-center align-self-end reset">
-                    <div class="badge-notify">
-                        82
+            <div class="row noti">
+                <div class="noti-event-image col-3 p-0">
+                    <img class="img-responsive" src="./img/locandina.jpg" alt="image of the event" />
+                </div>
+                <div class="noti-event-info col-7 padding-0">
+                    <p class="noti-event-date ">GIO. 12. DIC</p>
+                    <p class="noti-event-name text-truncate">HAPPY NEW YEAR 2020</p>
+                    <p class="noti-event-description text-truncate">Lorem ipsum as as ssss ciao ciccio</p>
+                </div>
+                <div class="col-2 reset">
+                    <div class="row reset">
+                        <div class="col-12 align-self-start reset noti-notice-date">12:03</div>
+                        <div class="col-12 align-self-center align-self-end reset">
+                            <div class="badge-notify">
+                                82
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
