5253e82c48419fc051662352457b17bd9e7e2f2a
cricas <cristian.casadei6@studio.unibo.it>
2020-01-17 19:12:35 +0100

    fix generale notifiche

--- template/cart.php
+++ template/cart.php
@@ -1,1 +1,41 @@
-<div class="col-12 button-orange"></div>
+<div class="col-12 contenuti">
+    <!-- HEADER -->
+    <div class="row">
+        <div class="col-12 sfondo-grigio">
+            <div class="row">
+                <div class="col-12">
+                    <p class="text-center">
+                        Totale provvisorio( 3 articoli): EUR 24,00
+                    </P>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <button type="button" class="button-orange">Procedi con l'ordine</button>
+                </div>
+            </div>
+        </div><!-- FINE HEADER -->
+
+        <!-- INZIO CONTENUTI -->
+        <div class="row noti">
+            <div class="noti-event-image col-3 p-0">
+                <img class="img-responsive" src="./img/locandina.jpg" alt="image of the event" />
+            </div>
+            <div class="noti-event-info col-7 padding-0">
+                <p class="noti-event-date ">GIO. 12. DIC</p>
+                <p class="noti-event-name text-truncate">HAPPY NEW YEAR 2020</p>
+                <p class="noti-event-description text-truncate">Lorem ipsum as as ssss ciao ciccio</p>
+            </div>
+            <div class="col-2 reset">
+                <div class="row reset">
+                    <div class="col-12 align-self-start reset noti-notice-date">12:03</div>
+                    <div class="col-12 align-self-center align-self-end reset">
+                        <div class="badge-notify">
+                            2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- template/notice.php
+++ template/notice.php
@@ -1,19 +1,22 @@
-<div class="col-11">
-    <div class="row contenuti">
-        <div class="container inline-block" id="notices">
+<div class="col-11 contenuti">
+    <div class="row inline-block" id="notices">
+        <div class=col-12>
+            <!-- inzio a inserire i contenuti -->
+
+            <!-- prima notifica-->
             <div class="row noti">
-                <div class="noti-event-image col-3 p-0">
+                <div class="noti-event-image col-3 pr-0 pt-0 pb-0">
                     <img class="img-responsive" src="./img/locandina.jpg" alt="image of the event" />
                 </div>
-                <div class="noti-event-info col-7 padding-0">
+                <div class="noti-event-info col-7">
                     <p class="noti-event-date ">GIO. 12. DIC</p>
                     <p class="noti-event-name text-truncate">HAPPY NEW YEAR 2020</p>
                     <p class="noti-event-description text-truncate">Lorem ipsum as as ssss ciao ciccio</p>
                 </div>
-                <div class="col-2 reset">
-                    <div class="row reset">
-                        <div class="col-12 align-self-start reset noti-notice-date">12:03</div>
-                        <div class="col-12 align-self-center align-self-end reset">
+                <div class="col-2">
+                    <div class="row">
+                        <div class="col-12 align-self-start noti-notice-date">12:03</div>
+                        <div class="col-12 align-self-center align-self-end">
                             <div class="badge-notify">
                                 2
                             </div>
@@ -21,19 +24,21 @@
                     </div>
                 </div>
             </div>
+
+            <!-- seconda notifica-->
             <div class="row noti">
-                <div class="noti-event-image col-3 p-0">
+                <div class="noti-event-image col-3 pr-0 pt-0 pb-0">
                     <img class="img-responsive" src="./img/locandina.jpg" alt="image of the event" />
                 </div>
-                <div class="noti-event-info col-7 padding-0">
+                <div class="noti-event-info col-7">
                     <p class="noti-event-date ">GIO. 12. DIC</p>
                     <p class="noti-event-name text-truncate">HAPPY NEW YEAR 2020</p>
                     <p class="noti-event-description text-truncate">Lorem ipsum as as ssss ciao ciccio</p>
                 </div>
-                <div class="col-2 reset">
-                    <div class="row reset">
-                        <div class="col-12 align-self-start reset noti-notice-date">12:03</div>
-                        <div class="col-12 align-self-center align-self-end reset">
+                <div class="col-2">
+                    <div class="row">
+                        <div class="col-12 align-self-start noti-notice-date">12:03</div>
+                        <div class="col-12 align-self-center align-self-end">
                             <div class="badge-notify">
                                 82
                             </div>
@@ -41,6 +46,8 @@
                     </div>
                 </div>
             </div>
-        </div>
+
+
+        </div><!-- fine contenti -->
     </div>
 </div>
