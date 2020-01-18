1a495db45ad54a9ee7756abfd01c450b574af50c
cricas <cristian.casadei6@studio.unibo.it>
2020-01-18 16:43:45 +0100

    fatta la pagina del carello e creata quella del pagamento

---
+++ pay.php
@@ -0,0 +1,11 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["titolo"] = "UniTicket - Pagamento";
+$templateParams["utente"] = "Michele";
+$templateParams["nome"] = "pay.php";
+$templateParams["sidebar"] = "sidebar.php";
+
+
+require("template/base.php");
+?>
