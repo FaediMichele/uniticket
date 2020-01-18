1f9c7b6eef273e1dd9383ceab23942e9ac0348e3
cricas <cristian.casadei6@studio.unibo.it>
2020-01-18 16:50:53 +0100

    fix css

--- template/cart.php
+++ template/cart.php
@@ -40,7 +40,7 @@
             <!--inizio seconda row-->
             <div class="row mt-2">
                 <div class="col-4 reset mx-auto">
-                    <div class="row justify-content-center select-quantity-cart">
+                    <div class="row justify-content-center">
                         <button type="button" class="p-0 select-quantity-cart-left text-white">
                             <div class="minus"></div>
                         </button>
@@ -83,7 +83,7 @@
             <!--inizio seconda row-->
             <div class="row mt-2">
                 <div class="col-4 reset mx-auto">
-                    <div class="row justify-content-center select-quantity-cart">
+                    <div class="row justify-content-center">
                         <button type="button" class="p-0 select-quantity-cart-left text-white">
                             <div class="minus"></div>
                         </button>
@@ -126,7 +126,7 @@
             <!--inizio seconda row-->
             <div class="row mt-2">
                 <div class="col-4 reset mx-auto">
-                    <div class="row justify-content-center select-quantity-cart">
+                    <div class="row justify-content-center">
                         <button type="button" class="p-0 select-quantity-cart-left text-white">
                             <div class="minus"></div>
                         </button>
