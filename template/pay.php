911ab721320a090bbe3b71dbf056bfa6eb806f11
cricas <cristian.casadei6@studio.unibo.it>
2020-01-19 13:43:22 +0100

    stato

--- template/pay.php
+++ template/pay.php
@@ -26,16 +26,14 @@
             <!--primo elemento-->
             <!--inizio prima row-->
             <div class="row">
-                <div class="col-4 p-0 text-left">
-                    <p class="text-white text-pay">x1</P>
+                <div class="col-4 text-left text-white text-pay">
+                    <p>x1</P>
                 </div>
-                <div class="col-4">
-                    <img class="image image-cart" src="./img/locandina.jpg">
+                <div class="col-4 fix-height">
+                    <img class="image image-pay" src="./img/locandina.jpg">
                 </div>
-                <div class="col-4">
-                    <p class="text-right text-red font-size-red">
-                        8€
-                    </p>
+                <div class="col-4 text-right text-red font-size-red text-pay-price">
+                    <p>8€</p>
                 </div>
             </div>
             <!--fine prima row-->
@@ -52,6 +50,67 @@
     </div>
     <!-- FINE PRIMO PRODOTTO -->
 
+
+    <!-- PRIMO PRODOTTO -->
+    <div class="row justify-content-center">
+        <div class="col-11 cart">
+            <!--primo elemento-->
+            <!--inizio prima row-->
+            <div class="row">
+                <div class="col-4 text-left text-white text-pay">
+                    <p>x1</P>
+                </div>
+                <div class="col-4 fix-height">
+                    <img class="image image-pay" src="./img/locandina.jpg">
+                </div>
+                <div class="col-4 text-right text-red font-size-red text-pay-price">
+                    <p>8€</p>
+                </div>
+            </div>
+            <!--fine prima row-->
+            <!--inizio seconda row-->
+            <div class="row mt-2">
+                <div class="col-12 reset mx-auto post">
+                    <h2>gio 12 dic</h2>
+                    <h4>energy <span>sala 2</span></h4>
+                    <h3>happy new year 2020 <span>Max Pezzali + Finley</span></h3>
+                </div>
+            </div>
+            <!--fine seconda row-->
+        </div>
+    </div>
+    <!-- FINE PRIMO PRODOTTO -->
+
+
+    <!-- PRIMO PRODOTTO -->
+    <div class="row justify-content-center">
+        <div class="col-11 cart">
+            <!--primo elemento-->
+            <!--inizio prima row-->
+            <div class="row">
+                <div class="col-4 text-left text-white text-pay">
+                    <p>x1</P>
+                </div>
+                <div class="col-4 fix-height">
+                    <img class="image image-pay" src="./img/locandina.jpg">
+                </div>
+                <div class="col-4 text-right text-red font-size-red text-pay-price">
+                    <p>8€</p>
+                </div>
+            </div>
+            <!--fine prima row-->
+            <!--inizio seconda row-->
+            <div class="row mt-2">
+                <div class="col-12 reset mx-auto post">
+                    <h2>gio 12 dic</h2>
+                    <h4>energy <span>sala 2</span></h4>
+                    <h3>happy new year 2020 <span>Max Pezzali + Finley</span></h3>
+                </div>
+            </div>
+            <!--fine seconda row-->
+        </div>
+    </div>
+    <!-- FINE PRIMO PRODOTTO -->
 
 
 </div>
