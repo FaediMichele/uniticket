393f07d9a2d15131a7afd47878d9c51f451eee5d
Michele Faedi <Michele.Faedi@studio.unibo.it>
2020-01-20 21:15:15 +0100

    corrected the notice

--- template/notice.php
+++ template/notice.php
@@ -3,13 +3,13 @@
         <div class=col-12>
             <?php
             $data = $dbh->getNotice($_COOKIE["sessionId"]);
-            var_dump($data);
+            
             foreach ($data as $key => $value){
+                $date = new Datetime($value["NoticeDate"]);
+                
                 ?><div class="row noti">
                 <div class="noti-event-image col-3 pr-0 pt-0 pb-0">
-                    data:image/jpeg;base64,'.'"/>';
-                    <img class="img-responsive" src="data:image/jpeg;base64,<?php echo base64_encode($value['img']) ?>"
-                        alt="image of the event" />
+                    <img class="img-responsive" src="<?php echo $value['img'] ?>" alt="image of the event" />
                 </div>
                 <div class="noti-event-info col-7">
                     <h3 class="noti-event-date mb-1"><?php echo $value["date"] ?>
@@ -20,7 +20,7 @@
                 </div>
                 <div class="col-2">
                     <div class="row">
-                        <div class="col-12 align-self-start noti-notice-date"><?php echo $value["NoticeDate"] ?>
+                        <div class="align-self-start noti-notice-date"><?php echo $date->format('m/d H:i'); ?>
                         </div>
                     </div>
                 </div>
