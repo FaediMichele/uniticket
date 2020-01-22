b406ef439a7e4c9cb69d1206af4e2a47afa49b7c
EmanueleBenedettini <emanuele.benedettini@studio.unibo.it>
2020-01-22 13:42:33 +0100

    orders and managed events query now works

--- template/eventManagement.php
+++ template/eventManagement.php
@@ -1,6 +1,53 @@
 <div class="col-12 contenuti border-bottom-son">
 
-    <!-- PRIMO PRODOTTO - ->
+    <?php 
+		$eventi = $templateParams["eventi"];
+		//$eventi[] = 1;	//togliere il commento per aggiungere un evento di test (questa riga fa la push dell' idEvento 1 nell'array $eventi)
+		foreach($eventi as $evento): 
+			$evento = $evento[0];
+			$event = $dbh->getEventInfo($evento)[0];
+			$location = $dbh->getRoomData($evento)[0];
+			$img = $dbh->getEventImages($evento);
+			$date = new Datetime($event["date"]);
+			/*foreach ($event as $key => $value) {
+				echo "Key: $key; Value: $value\n";
+			}*/
+	?>
+		<!-- EVENT -->
+		<div class="row justify-content-center">
+			<div class="col-11">
+				<!--primo elemento-->
+				<!--inizio prima row-->
+				<div class="row">
+				<div class="col-2 font-size-red text-vertical-center-father height-100 p-0"> </div>
+					<div class="col-8 height-100 text-vertical-center-father p-0">
+						<p class="text-vertical-center-son text-center">
+							<img src="<?php echo $img[0]["img"] ?>"
+                                    alt="immagine evento: <?php echo $event["name"]; ?>" />
+						</p>
+					</div>
+					<div class="col-2 text-vertical-center-father height-100 p-0">
+						<p class="text-red font-size-red text-vertical-center-son text-center"><?php echo $event["price"]; ?>€</p>
+					</div>
+				</div>
+				<!--fine prima row-->
+				<!--inizio seconda row-->
+				<div class="row mt-2">
+					<div class="col-12 reset mx-auto post">
+						<h2><?php echo $date->format('l d/m'); ?></h2>
+						<h4><?php echo $location["locationName"]; ?> <span><?php $location["roomName"]; ?></span></h4>
+						<h3><?php echo $event["eventName"]; ?> <span><?php echo $event["artist"]; ?></span></h3>
+					</div>
+				</div>
+				<!--fine seconda row-->
+			</div>
+		</div>
+	<?php 
+		endforeach; 
+	?>
+
+
+	<!-- PRIMO PRODOTTO - ->
     <div class="row justify-content-center">
         <div class="col-11">
             <!--primo elemento-->
@@ -31,51 +78,6 @@
         </div>
     </div>
     <!-- FINE PRIMO PRODOTTO -->
-
-    
-	<?php 
-		$eventi = $templateParams["eventi"];
-		$eventi[] = 1;	//togliere il commento per aggiungere un evento di test (questa riga fa la push dell' idEvento 1 nell'array $eventi)
-		foreach($eventi as $evento): 
-			$event = $dbh->getEventInfo($evento)[0];
-			$location = $dbh->getRoomData($evento)[0];
-			$img = $dbh->getEventImages($evento);
-			$date = new Datetime($event["date"]);
-			/*foreach ($event as $key => $value) {
-				echo "Key: $key; Value: $value\n";
-			}*/
-	?>
-		<!-- EVENT -->
-		<div class="row justify-content-center">
-			<div class="col-11">
-				<!--primo elemento-->
-				<!--inizio prima row-->
-				<div class="row">
-				<div class="col-2 font-size-red text-vertical-center-father height-100 p-0"> </div>
-					<div class="col-8 height-100 text-vertical-center-father p-0">
-						<p class="text-vertical-center-son text-center">
-							<img class="image image-pay " src="./img/locandina.jpg">	<!--load first image-->
-						</p>
-					</div>
-					<div class="col-2 text-vertical-center-father height-100 p-0">
-						<p class="text-red font-size-red text-vertical-center-son text-center"><?php echo $event["price"]; ?>€</p>
-					</div>
-				</div>
-				<!--fine prima row-->
-				<!--inizio seconda row-->
-				<div class="row mt-2">
-					<div class="col-12 reset mx-auto post">
-						<h2><?php echo $date->format('l d/m'); ?></h2>
-						<h4><?php echo $location["locationName"]; ?> <span><?php $location["roomName"]; ?></span></h4>
-						<h3><?php echo $event["name"]; ?> <span><?php echo $event["artist"]; ?></span></h3>
-					</div>
-				</div>
-				<!--fine seconda row-->
-			</div>
-		</div>
-	<?php 
-		endforeach; 
-	?>
 
 </div>
 
