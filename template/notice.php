625ae502ecd1166265211dfd2e14835460df3df3
cricas <cristian.casadei6@studio.unibo.it>
2020-01-23 10:29:23 +0100

    notice

--- template/notice.php
+++ template/notice.php
@@ -1,7 +1,12 @@
 <div class="col-11 contenuti">
     <div class="row inline-block" id="notices">
         <div class="col-12 container">
+            <!-- inzio contenitore  -->
+
+            <!-- contenitore naed -->
             <div class="panel-group">
+
+
                 <?php
                 $data = $dbh->getNotice($_COOKIE["sessionId"]);
                 for($i=0; $i < count($data); $i++){
@@ -41,7 +46,32 @@
                     </div>
                 </div>
                 <?php } ?>
+                <!-- contenitore naed -->
             </div>
+
+
+            <!-- prima notifica-->
+            <div class=" row noti">
+                <div class="noti-event-image col-3 pr-0 pt-0 pb-0">
+                    <img class="img-responsive" src="./img/locandina.jpg" alt="image of the event" />
+                </div>
+                <div class="noti-event-info col-7">
+                    <h3 class="noti-event-date mb-1">GIO. 12. DIC</h3>
+                    <h4 class="noti-event-name text-truncate mb-0">HAPPY NEW YEAR 2020</h4>
+                    <p class="noti-event-description text-truncate">Lorem ipsum as as ssss ciao ciccio</p>
+                </div>
+                <div class="col-2">
+                    <div class="row">
+                        <div class="col-12 align-self-start noti-notice-date">12:03</div>
+                        <div class="col-12 align-self-center align-self-end">
+                            <div class="badge-notify">2</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- fine prima notifica -->
+
+            <!-- fine contenitore  -->
         </div>
     </div>
 </div>
