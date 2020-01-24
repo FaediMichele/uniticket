af969927d1681a0f30ee8172847299b7c73d41ce
Emanuele Benedettini <emanuele.benedettini@studio.unibo.it>
2020-01-24 10:21:35 +0100

    partial cart management

--- cart.php
+++ cart.php
@@ -5,6 +5,8 @@ $templateParams["titolo"] = "UniTicket - Carrello";
 $templateParams["nome"] = "cart.php";
 $templateParams["sidebar"] = "sidebar.php";
 
+$templateParams["cart"] = $dbh->getEventsInCart($_COOKIE["sessionId"]);
+
 require("template/base.php");
 
 $dbh->close();
