1173bd483183f9142c5b54ce8b6f617d8eed623d
cricas <cristian.casadei6@studio.unibo.it>
2020-01-25 09:16:15 +0100

    fix home

--- template/home.php
+++ template/home.php
@@ -13,62 +13,58 @@
 					echo "Key: $key; Value: $value\n";
 				}*/
 		?>
-
-        <div class="col-12 col-xl-3 home-post">
-            <article class="row">
-                <header class="col-12">
-                    <div id="event-<?php echo $index; ?>" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#event-<?php echo $index; ?>" data-slide-to="0" class="active">
-                            </li>
-                            <?php for ($i = 1; $i < count($img); $i++) { ?>
-                            <li data-target="#event-<?php echo $index; ?>" data-slide-to="<?php echo $i ?>">
-                            </li>
-                            <?php } ?>
-                        </ol>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="<?php echo $img[0]["img"] ?>"
-                                    alt="immagine evento: <?php echo $event["eventName"]; ?>" />
-                            </div>
-                            <?php
+        <article class="col-12 col-xl-3 mb-2 home-post">
+            <a href="eventInfo.php?ID=<?php echo $evento ?>">
+                <div class="row">
+                    <header class="col-12">
+                        <div id="event-<?php echo $index; ?>" class="carousel slide " data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#event-<?php echo $index; ?>" data-slide-to="0" class="active">
+                                </li>
+                                <?php for ($i = 1; $i < count($img); $i++) { ?>
+                                <li data-target="#event-<?php echo $index; ?>" data-slide-to="<?php echo $i ?>">
+                                </li>
+                                <?php } ?>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="<?php echo $img[0]["img"] ?>"
+                                        alt="immagine evento: <?php echo $event["eventName"]; ?>"
+                                        class="cover-100-percent" />
+                                </div>
+                                <?php
 								for ($i = 1; $i < count($img); $i++) {
 									
 									$value = $img[$i]; ?>
-                            <div class="carousel-item">
-                                <img src="<?php echo $value["img"] ?>"
-                                    alt="immagine evento: <?php echo $event["eventName"]; ?>" />
+                                <div class="carousel-item">
+                                    <img src="<?php echo $value["img"] ?>"
+                                        alt="immagine evento: <?php echo $event["eventName"]; ?>" />
+                                </div>
+                                <?php } ?>
                             </div>
+                            <?php if(count($img) > 1){ ?>
+                            <a class="carousel-control-prev" href="#event-<?php echo $index; ?>" role="button"
+                                data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#event-<?php echo $index; ?>" role="button"
+                                data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
                             <?php } ?>
                         </div>
-                        <?php if(count($img) > 1){ ?>
-                        <a class="carousel-control-prev" href="#event-<?php echo $index; ?>" role="button"
-                            data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#event-<?php echo $index; ?>" role="button"
-                            data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                        <?php } ?>
-                    </div>
-                    <div class="info">
-                        <h2><?php echo $date->format('l d/m'); ?></h2>
-                        <h4><?php echo $event["locationName"]; ?> - <span><?php echo $event["roomName"]; ?></span>
-                        </h4>
-                        <h3><?php echo $event["eventName"]; ?> <span><?php echo $event["artist"]; ?></span></h3>
-                    </div>
-                </header>
-
-                <footer>
-					<a href="eventInfo.php?ID=<?php echo $evento ?>">Dettagli evento</a>
-                </footer>
-            </article>
-        </div>
-
+                        <div>
+                            <h2><?php echo $date->format('l d m'); ?></h2>
+                            <h4><?php echo $event["locationName"]; ?> - <span><?php echo $event["roomName"]; ?></span>
+                            </h4>
+                            <h3><?php echo $event["eventName"]; ?> <span><?php echo $event["artist"]; ?></span></h3>
+                        </div>
+                    </header>
+                </div>
+            </a>
+        </article>
         <?php } ?>
-
     </div>
 </div>
