67c198d85116a71972c8cfd9a3a77bf43427f4e9
Michele Faedi <Michele.Faedi@studio.unibo.it>
2020-02-03 14:50:02 +0100

    aggiunta la gestione dei luoghi tramite un menu collapse nella sidebar avanzata. Inizio la creazione del locale

---
+++ createLocation.php
@@ -0,0 +1,13 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["titolo"] = "UniTicket - Home";
+$templateParams["nome"] = "createLocation.php";
+$templateParams["sidebar"] = "sidebar.php";
+
+//$templateParams["eventi"] = $dbh->getEvents();
+$templateParams["eventi"] = $dbh->getUpcomingEvents($_COOKIE["sessionId"]);
+require("template/base.php");
+
+$dbh->close();
+?>
--- template/sidebarAdvanced.php
+++ template/sidebarAdvanced.php
@@ -10,6 +10,17 @@
         <a href="createEvent.php">Organizza un nuovo evento</a>
     </li>
     <li>
+        <a data-toggle="collapse" href="#placeManagementSidebar" role="button">Gestione luoghi</a>
+        <div class="collapse" id="placeManagementSidebar">
+            <div class="ml-5">
+                <a href="createLocation.php">Crea nuovo locale</a>
+                <a href="">Crea nuova sala</a>
+                <a href="">Modifica locale</a>
+                <a href="">Modifica sala</a>
+            </div>
+        </div>
+    </li>
+    <li>
         <a href="eventManagement.php">Lista Eventi organizzati</a>
     </li>
 </ul>
