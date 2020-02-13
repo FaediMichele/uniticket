30889d2ac848a8ef63e249961a325e727c994137
cricas <cristian.casadei6@studio.unibo.it>
2020-02-13 18:10:04 +0100

    deskopizzato il carrello

--- template/cart.php
+++ template/cart.php
@@ -9,8 +9,8 @@
                     </P>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-12">
+            <div class="row justify-content-center">
+                <div class="col-12 col-sm-8 col-md-6 col-lg-4 col-xl-3">
                     <button type="submit" id="checkout" name="checkout" value="checkout" onclick="checkout()"
                         class="button-orange text-uppercase">Procedi con l'ordine</button>
                 </div>
@@ -29,28 +29,28 @@
 			$img = $dbh->getEventImages($evento);
 			$date = new Datetime($event["date"]);
 		?>
-        <!-- PRIMO PRODOTTO -->
+        <!--PRODOTTO -->
         <div class="row justify-content-center">
-            <div class="col-11 cart">
+            <div class="col-11 col-lg-10 col-xl-9 cart">
                 <!--primo elemento-->
                 <!--inizio prima row-->
                 <div class="row">
-                    <div class="col-4 p-0 text-center">
+                    <div class="col-4 col-sm-3 col-md-2 col-xl-2 p-0 text-center">
                         <img class="cover-95" src="<?php echo $img[0]["img"] ?>"
                             alt="immagine evento: <?php echo $event["name"]; ?>" />
                     </div>
-                    <div class="col-5">
+                    <div class="col-5 col-md-6 col-xl-8">
                         <h3 class="noti-event-date mb-1"><?php echo $date->format('l d/m'); ?></h3>
                         <h4 class="noti-event-name text-truncate mb-0"><?php echo $event["eventName"]; ?></h4>
                     </div>
-                    <div class="col-3 text-right">
+                    <div class="col-3 col-sm-4 col-xl-2 text-right">
                         <p class="text-red font-size-red"><?php echo $event["price"]; ?>€</p>
                     </div>
                 </div>
                 <!--fine prima row-->
                 <!--inizio seconda row-->
                 <div class="row mt-2">
-                    <div class="col-4 reset mx-auto">
+                    <div class="col-4 col-sm-3 col-md-2 col-xl-2 reset mx-auto">
                         <div class="row justify-content-center">
                             <button type="button" class="dec-<?php echo $evento ?> select-quantity-cart-left text-white"
                                 onclick="decrement('qt-<?php echo $evento ?>')">
@@ -66,7 +66,7 @@
                                 onclick="increment('qt-<?php echo $evento ?>')">+</button>
                         </div>
                     </div>
-                    <div class="col-8">
+                    <div class="col-8 col-sm-9 col-md-10 col-xl-10">
                         <button type="button"
                             class="button-red text-white text-uppercase delete-button-cart float-right">Rimuovi</button>
                     </div>
@@ -74,7 +74,7 @@
                 <!--fine seconda row-->
             </div>
         </div>
-        <!-- FINE PRIMO PRODOTTO -->
+        <!-- FINE PRODOTTO -->
 
         <?php 
 			endforeach; 
