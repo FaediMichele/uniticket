b221a34bbe26481851c20b56b813f544d807fbcd
Michele Faedi <Michele.Faedi@studio.unibo.it>
2020-02-15 10:10:30 +0100

    corretta la creazione degli eventi

--- eventManagement.php
+++ eventManagement.php
@@ -4,7 +4,7 @@ require_once("bootstrap.php");
 $templateParams["titolo"] = "UniTicket - Events";
 $templateParams["nome"] = "eventManagement.php";
 $templateParams["sidebar"] = "sidebar.php";
-
+$templateParams["events"] = $dbh->getManagedEvent($_COOKIE["sessionId"]);
 
 require("template/base.php");
 
--- template/eventManagement.php
+++ template/eventManagement.php
@@ -1,7 +1,7 @@
 <div class="col-12 contenuti border-bottom-son">
 
     <?php 
-		$eventi = $templateParams["eventi"];
+		$eventi = $templateParams["events"];
 		//$eventi[] = 1;	//togliere il commento per aggiungere un evento di test (questa riga fa la push dell' idEvento 1 nell'array $eventi)
 		foreach($eventi as $evento): 
 			$evento = $evento[0];
@@ -10,45 +10,45 @@
 			$img = $dbh->getEventImages($evento);
 			$date = new Datetime($event["date"]);
 	?>
-		<!-- EVENT -->
-		<div class="row justify-content-center">
-			<div class="col-11">
-				<!--primo elemento-->
-				<!--inizio prima row-->
-				<div class="row">
-				<div class="col-2 font-size-red text-vertical-center-father height-100 p-0"> </div>
-					<div class="col-8 height-100 text-vertical-center-father p-0">
-						<p class="text-vertical-center-son text-center">
-							<img src="<?php echo $img[0]["img"] ?>"
-                                    alt="immagine evento: <?php echo $event["name"]; ?>" />
-						</p>
-					</div>
-					<div class="col-2 text-vertical-center-father height-100 p-0">
-						<p class="text-red font-size-red text-vertical-center-son text-center"><?php echo $event["price"]; ?>€</p>
-					</div>
-				</div>
-				<!--fine prima row-->
-				<!--inizio seconda row-->
-				<div class="row mt-2">
-					<div class="col-12 reset mx-auto post">
-						<h2><?php echo $date->format('l d/m'); ?></h2>
-						<h4><?php echo $location["locationName"]; ?> <span><?php $location["roomName"]; ?></span></h4>
-						<h3><?php echo $event["eventName"]; ?> <span><?php echo $event["artist"]; ?></span></h3>
-					</div>
-				</div>
-				<!--fine seconda row-->
-			</div>
-		</div>
-	<?php 
+    <!-- EVENT -->
+    <div class="row justify-content-center">
+        <div class="col-11">
+            <!--primo elemento-->
+            <!--inizio prima row-->
+            <div class="row">
+                <div class="col-2 font-size-red text-vertical-center-father height-100 p-0"> </div>
+                <div class="col-8 height-100 text-vertical-center-father p-0">
+                    <p class="text-vertical-center-son text-center">
+                        <img src="<?php echo $img[0]["img"] ?>" alt="immagine evento: <?php echo $event["name"]; ?>" />
+                    </p>
+                </div>
+                <div class="col-2 text-vertical-center-father height-100 p-0">
+                    <p class="text-red font-size-red text-vertical-center-son text-center"><?php echo $event["price"]; ?>€</p>
+                </div>
+            </div>
+            <!--fine prima row-->
+            <!--inizio seconda row-->
+            <div class="row mt-2">
+                <div class="col-12 reset mx-auto post">
+                    <h2><?php echo $date->format('l d/m'); ?></h2>
+                    <h4><?php echo $location["locationName"]; ?> <span><?php $location["roomName"]; ?></span></h4>
+                    <h3><?php echo $event["eventName"]; ?> <span><?php echo $event["artist"]; ?></span></h3>
+                </div>
+            </div>
+            <!--fine seconda row-->
+        </div>
+    </div>
+    <?php 
 		endforeach; 
 	?>
 
 
-	<!-- PRIMO PRODOTTO - ->
+    <!-- PRIMO PRODOTTO - ->
     <div class="row justify-content-center">
         <div class="col-11">
             <!--primo elemento-->
-            <!--inizio prima row--><!--
+    <!--inizio prima row-->
+    <!--
             <div class="row">
                 <div class="col-2 font-size-red text-vertical-center-father height-100 p-0">
                     <p class="text-vertical-center-son text-white text-center">x1</p>
@@ -63,7 +63,8 @@
                 </div>
             </div>
             <!--fine prima row-->
-            <!--inizio seconda row--><!--
+    <!--inizio seconda row-->
+    <!--
             <div class="row mt-2">
                 <div class="col-12 reset mx-auto post">
                     <h2>gio 12 dic</h2>
@@ -71,10 +72,10 @@
                     <h3>happy new year 2020 <span>Max Pezzali + Finley</span></h3>
                 </div>
             </div>
-            <!--fine seconda row--><!--
+            <!--fine seconda row-->
+    <!--
         </div>
     </div>
     <!-- FINE PRIMO PRODOTTO -->
 
 </div>
-
