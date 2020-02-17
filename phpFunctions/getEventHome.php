df8963be9dc230d94b6a8a9bcfc110ea22217511
Michele Faedi <Michele.Faedi@studio.unibo.it>
2020-02-17 19:27:19 +0100

    .

--- phpFunctions/getEventHome.php
+++ phpFunctions/getEventHome.php
@@ -71,11 +71,24 @@ $eventi = $dbh->getUpcomingEvents($_COOKIE["sessionId"], 10, $_POST["offset"]);
 <?php
     }
     if(count($eventi) < $_POST["offset"]){
-        echo '<div class="separate">
-            <div class="circle"></div>
-            <div class="circle"></div>
-            <div class="circle"></div>
-        </div><h3 class="text-center">Non ci sono ulteriori eventi da visualizzare</h3>';
+        echo '
+        <div class="col-12 mt-1 mb-1">
+            <div class="row">
+                <div class="col-12 mt-2 mb-2">
+                    <div class="separate">
+                        <div class="circle"></div>
+                        <div class="circle"></div>
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 text-center">
+                    <p class="text-center text-orange">Non ci sono ulteriori eventi da visualizzare</p>
+                </div>
+            </div>
+        </div>';
     }
 $dbh->close();
 ?>
