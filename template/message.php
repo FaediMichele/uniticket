3485c56c0bb9ea803a19bb1fdbb25da7bc09fbf1
cricas <cristian.casadei6@studio.unibo.it>
2020-02-17 15:57:20 +0100

    fix messagge

--- template/message.php
+++ template/message.php
@@ -1,8 +1,13 @@
 <div class="col-12 contenuti">
-    <h2 class="text-center"><?php echo $_GET["message"]; ?></h2>
-    <?php
+    <div class="row">
+        <h2 class="col-12 text-center text-uppercase"><?php echo $_GET["message"]; ?></h2>
+    </div>
+    <div class="row justify-content-center mt-5">
+        <?php
         if(isset($_GET["link"]) && isset($_GET["linkName"])){
-            echo '<a class="button-orange text-center" href="' . $_GET["link"] . '">' . $_GET["linkName"] . '</a>';
+            echo '<button onclick="location.href =' . $_GET["link"] .' ;" class="button-orange text-uppercase col-12 col-sm-8 col-md-6 col-lg-4 col-xl-3">' . $_GET["linkName"] . '</button>
+            ';
         }
     ?>
+    </div>
 </div>
