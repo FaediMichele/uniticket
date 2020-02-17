091b2b168c5f4f81482e53a4d97b9922500982e4
cricas <cristian.casadei6@studio.unibo.it>
2020-02-17 16:07:50 +0100

    fix message

--- template/message.php
+++ template/message.php
@@ -2,12 +2,11 @@
     <div class="row">
         <h2 class="col-12 text-center text-uppercase"><?php echo $_GET["message"]; ?></h2>
     </div>
-    <div class="row justify-content-center mt-5">
-        <?php
+    <?php
         if(isset($_GET["link"]) && isset($_GET["linkName"])){
-            echo '<button onclick="location.href =' . $_GET["link"] .' ;" class="button-orange text-uppercase col-12 col-sm-8 col-md-6 col-lg-4 col-xl-3">' . $_GET["linkName"] . '</button>
-            ';
+            echo '<a class="row justify-content-center mt-5" href="' . $_GET["link"] . '">
+                <button class="button-orange text-uppercase col-12 col-sm-8 col-md-6 col-lg-4 col-xl-3">' . $_GET["linkName"] . '</button>
+            </a>';
         }
     ?>
-    </div>
 </div>
