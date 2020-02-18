d58852ec3d9f33c39f92f8840023c2c91688af52
Michele Faedi <Michele.Faedi@studio.unibo.it>
2020-02-18 14:03:26 +0100

    corretto in parte la home

--- template/base.php
+++ template/base.php
@@ -101,6 +101,7 @@
                         <div class="col-12">
                             <form id="form-search" action="phpFunctions/search.php" method="POST" enctype="multipart/form-data">
                                 <span id="searchLogo" class="icon"><i class="fa fa-search"></i></span>
+                                <label for="search" class="only-sr">Search</label>
                                 <input type="search" name="sitesearch" placeholder="Search" class="input input-max-width" id="search" oninput='searching()' />
                             </form>
                         </div>
